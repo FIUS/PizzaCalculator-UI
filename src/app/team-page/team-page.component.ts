601f1bfcca9f641e7a05ca3900c97ceb0f1f012b
Heiko Geppert <heiko.geppert@outlook.de>
2019-01-12 12:44:22 +0100

    Add teamPage skeleton

--- src/app/team-page/team-page.component.ts
+++ src/app/team-page/team-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-team-page',
   templateUrl: './team-page.component.html',
   styleUrls: ['./team-page.component.css']
 })
-export class TeamPageComponent implements OnInit {
+export class TeamPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, ) { }
+
+  subParam: Subscription;
+
+  teamName: string;
 
   ngOnInit() {
+    this.subParam = this.route.params.subscribe(params => {
+      this.teamName = params['teamName'];
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subParam != null) {
+      this.subParam.unsubscribe();
+    }
   }
 
 }
