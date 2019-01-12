7bc587da4ba6bb26bf26c83f1db7e5c1705b4e77
Heiko Geppert <heiko.geppert@outlook.de>
2019-01-12 12:24:50 +0100

    Add material and tachyons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { TeamPageComponent } from './team-page/team-page.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 
+import {
+  MatButtonModule, MatButtonToggleModule, MatCardModule, MatMenuModule,
+  MatIconModule, MatListModule, MatSidenavModule, MatDialogModule, MatToolbarModule,
+  MatTooltipModule, MatTableModule, MatInputModule, MatTabsModule, MatChipsModule, MatSlideToggleModule, MatSelectModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +25,28 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
     AdminPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // material
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatInputModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
