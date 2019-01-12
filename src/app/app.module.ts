5058a2f14c17d21ef6f276eb041b9e19eb0aac32
Heiko Geppert <heiko.geppert@outlook.de>
2019-01-12 12:13:10 +0100

    Add page dummys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TeamPageComponent } from './team-page/team-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    TeamPageComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule
